d0284b4cec940b680e5489a24746b726566dadd2
Weslley Cardoso <weslleycardoso011@gmail.com>
2026-02-12 21:00:29 -0300

    Chore: Initial project setup (Spring Boot + React)

---
+++ backend/src/main/java/com/apexfit/backend/ApexFitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.apexfit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexFitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexFitBackendApplication.class, args);
+	}
+
+}
