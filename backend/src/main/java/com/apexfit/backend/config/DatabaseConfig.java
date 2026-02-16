1e9fd6faea8328fefa6ce01e1d951bb8a2f9d303
Weslley Cardoso <weslleycardoso011@gmail.com>
2026-02-16 16:41:15 -0300

    chore: temp restore DatabaseConfig to force index sync

---
+++ backend/src/main/java/com/apexfit/backend/config/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package com.apexfit.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+    // This file is temporary and will be deleted.
+}
